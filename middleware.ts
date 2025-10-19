3baff3a1811070556480f6f9f5322d9250f83454
unknown <shrivastavaaryan762@gmail.com>
2025-10-20 02:22:36 +0530

    logo fixed

--- middleware.ts
+++ middleware.ts
@@ -50,6 +50,6 @@ export default auth((req) => {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|logo.png|luxury.jpg|hero-image-logotype.png|logoo.png).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|logo.png|luxury.jpg|hero-image-logotype.png|logoo.png|logooo.png).*)",
   ],
 };
